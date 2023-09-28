c7ddc30bb48b0d46def74ad6921e5c72932ce19b
Tomás Ó Dálaigh <tom1996.18@gmail.com>
2023-09-29 00:32:52 +0100

    Log city, we add many logs

--- main.py
+++ main.py
@@ -23,49 +23,65 @@ def version_lower_than_or_equal(ver, target):
     target_v = version.parse(target)
     return (ver_v < target_v or ver_v == target_v)
 
-
 def cron_string(ver):
-    return str(random.randint(0, 59)) + ' ' + str(random.randint(0, 23)) + ' ' + "*/" + str(random.randint(13, 14)) + " * *"
-
-# Modify fix_cron_and_interval to accept filename
-def fix_cron_and_interval(test, filename):
-    if 'cron' in test and 'interval' in test:
-        # Add the filename to a .txt file
-        with open("files-with-cronAndInterval.txt", "a") as f:
-            f.write(f"{filename}\n")
-        del test['interval']  # Delete the 'interval' field
-        return True  # Return True if changes are made
-    return False  # Return False otherwise
+    logging.info("We entered cron_string")
+    if ver == "4.13":
+        # Weekly (Saturday or Sunday)
+        return f"{random.randint(0, 59)} {random.randint(0, 23)} * * {random.choice([6, 7])}"
+    # COMMENTED OUT LINE AS 4.12 will be ignored.
+    # elif ver == "4.12":
+    #     # Bi-weekly (existing, so no change)
+    #     return f"{random.randint(0, 59)} {random.randint(0, 23)} */14 * *"
+    else:
+        # Older than 4.12, bi-weekly or monthly
+        return f"{random.randint(0, 59)} {random.randint(0, 23)} */{random.choice([14, 30])} * *"
 
 def process_interval(test, ver):
+    logging.info("We entered process_interval")
     changes_made = []
     # Get the name of the test from 'as' or 'name' fields
     name = test['as'] if 'as' in test else test['name']
-    logging.info(f'found test {name} with interval {test["interval"]}')
-
-    if name.startswith('promote-'):
-        logging.info(f'found promote test {name}')
+    
+    # If these conditions are met, log and return an empty list
+    if ver == "4.12" or name.startswith("promote-") or name.startswith("mirror-nightly-image"):
+        if ver == "4.12":
+            logging.info(f"Found and ignored 4.12 {name}")
+        if name.startswith("promote-"):
+            logging.info(f"Found and ignored promote test {name}")
+        if name.startswith("mirror-nightly-image"):
+            logging.info(f"Found and ignored mirror-nightly-image test {name}")
         return []
+
+    logging.info(f'Found test in {name} with interval {test["interval"]}')
+
+    # Continue with the original logic
+    if 'interval' in test:
+        interval = test['interval'].strip()
         
-    interval = test['interval'].strip()
-    
-    # Remove the interval if both cron and interval exist
-    if 'cron' in test:
-        del test['interval']
-        changes_made.append(f"Removed interval for {name}")
-    
-    # Replace the interval with a cron value based on the version number if only interval exists
-    elif 'cron' not in test:
-        del test['interval']
-        test['cron'] = cron_string(ver)  # Assuming cron_string() can accept a version number
-        changes_made.append(f"Replaced interval with cron for {name}")
+        if 'cron' in test:
+            del test['interval']
+            changes_made.append(f"Removed interval for {name}")
+        elif 'cron' not in test:
+            del test['interval']
+            test['cron'] = cron_string(ver)
+            changes_made.append(f"Replaced interval with cron for {name}")
 
     return changes_made
 
 def process_cron(test, ver):
+    logging.info("We entered process_cron")
     changes_made = []
     name = test['as'] if 'as' in test else test['name']
-    logging.info(f'found test \033[94m{name}\033[0m with cron \033[92m{test["cron"]}\033[0m')
+    logging.info(f'Found test in \033[94m{name}\033[0m with cron \033[92m{test["cron"]}\033[0m')
+
+    if ver == "4.12" or name.startswith("promote-") or name.startswith("mirror-nightly-image"):
+        if ver == "4.12":
+            logging.info(f"Found and ignored 4.12 {name}")
+        if name.startswith("promote-"):
+            logging.info(f"Found and ignored promote test {name}")
+        if name.startswith("mirror-nightly-image"):
+            logging.info(f"Found and ignored mirror-nightly-image test {name}")
+        return []
     
     # Update the cron based on the version_number
     test["cron"] = cron_string(ver)  # Assuming cron_string() can accept a version number
@@ -74,6 +90,7 @@ def process_cron(test, ver):
     return changes_made
 
 def process_promote(test):
+    logging.info("We entered process_promote")
     changes_made = []
     name = test['as'] if 'as' in test else test['name']
     logging.info(f'Found promote test {name}')
@@ -85,11 +102,14 @@ def process_promote(test):
 
     return changes_made
 
-def replace(test, filename, ver):
+def replace(test, ver, filename):
     changes_made = []
 
-    if fix_cron_and_interval(test, filename):
-        changes_made.append("fixed_cron_and_interval")
+    logging.info(f"We entered replace for file: {filename}")
+
+    # Skip if version is 4.12
+    if ver == "4.12":
+        return changes_made
 
     name = test['as'] if 'as' in test else test['name']
     
@@ -100,15 +120,15 @@ def replace(test, filename, ver):
     elif 'cron' in test:
         changes_made.extend(process_cron(test, ver))
         
-    # TODO: Edit this section later for other responsibilities
-    
     return changes_made
 
-
 def process_ciops(data, filename):
+    logging.info("We entered process_ciops")
+    logging.info(f"Number of tests in {filename}: {len(data.get('tests', []))}")
     section_latest = data.get('releases', {}).get('latest', {})
     if not section_latest:
-        return False
+        return []
+    
     release_ref = list(section_latest.keys())[0]
 
     if 'version' in section_latest[release_ref]:
@@ -118,18 +138,39 @@ def process_ciops(data, filename):
     elif 'version_bounds' in section_latest[release_ref]:
         ver = section_latest[release_ref].get('version_bounds', {}).get('upper')
 
+    # Skip if version is 4.12
+    if ver == "4.12":
+        logging.info(f"Skipping file {filename} with version 4.12")
+        return []
+
+    # Skip if filename starts with "promote-" or "mirror-nightly-image"
+    if filename.startswith("promote-") or filename.startswith("mirror-nightly-image"):
+        logging.info(f"Skipping file {filename} starting with promote- or mirror-nightly-image")
+        return []
+    
     if not ver or not version_lower_than_or_equal(ver, TARGET_VERSION):
-        return False
+        return []
 
     pending_replacements = []
     logging.info(f'Found version \033[91m{ver}\033[0m in \033[94m{filename}\033[0m')
     for test in data.get('tests', []):
-        pending_replacements.extend(replace(test, filename, ver))
+        logging.info(f"Processing test: {test.get('name', 'Unknown')} in file: {filename}")
+        pending_replacements.extend(replace(test, ver, filename))  # include filename here
 
+
+    print(f"Returning from process_ciops: {pending_replacements}, type: {type(pending_replacements)}")
     return pending_replacements
 
 # Processes 'job' data, replacing cron strings if they meet certain conditions.
 def process_job(data, filename):
+    logging.info("We entered process_job")
+    logging.info(f"Number of tests in {filename}: {len(data.get('periodics', []))}")
+
+    # Skip if filename starts with "promote-" or "mirror-nightly-image"
+    if filename.startswith("promote-") or filename.startswith("mirror-nightly-image"):
+        logging.info(f"Skipping file {filename} starting with promote- or mirror-nightly-image")
+        return []
+    
     for periodic in data.get('periodics', []):
         if 'ci.openshift.io/generator' in periodic.get('labels', {}):
             continue
@@ -144,6 +185,12 @@ def process_job(data, filename):
                 logging.info(f'unrecognised base_ref {base_ref}')
                 continue
             ver = base_ref[1]
+            
+            # Skip if version is 4.12
+            if ver == "4.12":
+                logging.info(f"Skipping due to version 4.12")
+                continue
+
             if ver and version_lower_than_or_equal(ver, TARGET_VERSION):
                 version_satisfied = True
                 break
@@ -154,31 +201,40 @@ def process_job(data, filename):
                 version_satisfied = True
 
         if not version_satisfied:
-            return False
+            return []
 
         pending_replacements = []
         logging.info(f'Found version {ver} lower than {TARGET_VERSION} in \033[94m{filename}\033[0m')
-        pending_replacements.extend(replace(periodic, filename, ver))
+        for periodic in data.get('periodics', []):
+            logging.info(f"Processing test: {periodic.get('name', 'Unknown')} in file: {filename}")
+            pending_replacements.extend(replace(periodic, filename, ver))  # include filename here
 
+
+        print(f"Returning from process_job: {pending_replacements}, type: {type(pending_replacements)}")
         return pending_replacements
-
+    return []
 
 if __name__ == '__main__':
     FILENAME = sys.argv[1]
-
+    
     with open(FILENAME, 'r', encoding='utf-8') as fp:
         ycontent = fp.read()
-
+    
     yaml = ruamel.yaml.YAML()
     pending = []
     all_data = list(yaml.load_all(ycontent))
     file_changed = False
+    
+    changes_made = []
     for data in all_data:
-        ret = process_ciops(data, FILENAME)
-        ret2 = process_job(data, FILENAME)
-        if ret or ret2:
+        changes1 = process_ciops(data, FILENAME)
+        changes2 = process_job(data, FILENAME)
+        if changes1 or changes2:
+            changes_made.extend(changes1)
+            changes_made.extend(changes2)
             file_changed = True
-
+    
     if file_changed:
+        # Apply your changes to all_data here
         with open(FILENAME, 'w', encoding='utf-8') as fp:
             yaml.dump_all(all_data, fp)
